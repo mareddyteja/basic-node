f0ca3210390d2cf855a42b8a42befe614eaf66aa
Teja Mahesh Reddy M <37553177+mareddyteja@users.noreply.github.com>
2022-09-26 16:12:50 +0530

    Update server.js
    remove unwanted changes

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-// var http = require('http')
-// var fs = require('fs')
-// http.createServer((res,req)=>{
-//     fs.readFile('./index.html', (err, data)=>{
-//         res.write(data)
-//         return res.end()
-//     })
-// }).listen(9000)
-
-
-
-
 const http = require('http');
 const fs = require('fs').promises;
 const requestListener = function (req, res) {
